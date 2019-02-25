64d17ef3d2d8795f94b716ffcc8b1a361b1ee485
Bianca Carlsen <bia.carlsen@gmail.com>
2019-02-25 14:56:01 -0300

    Baixando o tema para o projeto (git guiferreira/netflix)

--- introducao_ao_vuejs2/netflixVue/src/App.vue
+++ introducao_ao_vuejs2/netflixVue/src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <h2>Meu componente - Projeto: {{nomeProjeto}}</h2>
 </template>
+
+
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      nomeProjeto: 'Netflix com Vue'
+    }
+  }
 }
 </script>
 
